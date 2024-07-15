f05183c6cdbda82172f981ca20fb30a5c6cf1869
Jason Hansen <jasohansen@gmail.com>
2024-07-15 12:03:41 -0600

    blah

--- script/test.js
+++ script/test.js
@@ -2,18 +2,19 @@ async function get_data() {
     return await fetch("https://marvelsnap.pro/snap/do.php?cmd=getcards", {
         "headers": {
           "accept": "application/json, text/plain, */*",
-          "accept-language": "en-US,en;q=0.9",
-          "priority": "u=1, i",
-          "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": "\"Windows\"",
-          "sec-fetch-dest": "empty",
-          "sec-fetch-mode": "cors",
-          "sec-fetch-site": "same-origin",
+          // "accept-language": "en-US,en;q=0.9",
+          // "priority": "u=1, i",
+          // "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
+          // "sec-ch-ua-mobile": "?0",
+          // "sec-ch-ua-platform": "\"Windows\"",
+          // "sec-fetch-dest": "empty",
+          // "sec-fetch-mode": "cors",
+          // "sec-fetch-site": "same-origin",
           "Referer": "https://marvelsnap.pro/collection/",
-          "Referrer-Policy": "strict-origin-when-cross-origin"
+          // "Referrer-Policy": "strict-origin-when-cross-origin"
         },
         "body": null,
+        "mode":"no-cors",
         "method": "GET"
       });
     }
