ad769ea367d22729fe45397859f7c838f7f885ec
Jason Hansen <jasohansen@gmail.com>
2024-07-14 22:21:23 -0600

    fuzzy

--- script/card.js
+++ script/card.js
@@ -1,13 +1,14 @@
-// Function to fetch JSON data from the API
+// Function to fetch JSON data using a CORS proxy
 async function fetchData() {
-      
-    var response = await fetch("https://static2.marvelsnap.pro/snap/do.php?cmd=getcards", {
-        method: 'GET',
-        mode: 'no-cors'
-      });
-    var data = await response.json();
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const targetUrl = 'https://marvelsnap.pro/snap/do.php?cmd=getcards';
+    const response = await fetch(proxyUrl + targetUrl, {
+        method: "GET"
+    });
+    const data = await response.json();
     return data;
 }
+
 // Function to get the query parameter from the URL
 function getQueryParam(param) {
     const urlParams = new URLSearchParams(window.location.search);
@@ -17,7 +18,7 @@ function getQueryParam(param) {
 // Function to update the results
 function updateResults(query, data) {
     const keys = Object.keys(data);
-    let options = {
+    const options = {
         includeScore: true,
         keys: ['key']
     };
@@ -26,10 +27,10 @@ function updateResults(query, data) {
     const fuse = new Fuse(keys.map(key => ({ key })), options);
 
     // Perform the fuzzy search
-    let result = fuse.search(query);
+    const result = fuse.search(query);
 
     // Extract the best matched key from the results
-    let matchedKey = result.length > 0 ? result[0].item.key : null;
+    const matchedKey = result.length > 0 ? result[0].item.key : null;
 
     // Display the result
     const resultsDiv = document.getElementById('results');
@@ -45,7 +46,7 @@ function updateResults(query, data) {
 async function initSearch() {
     const query = getQueryParam('query');
     if (query) {
-        let data = await fetchData();
+        const data = await fetchData();
         updateResults(query, data);
     } else {
         document.getElementById('results').innerHTML = '<div>No query parameter provided</div>';
